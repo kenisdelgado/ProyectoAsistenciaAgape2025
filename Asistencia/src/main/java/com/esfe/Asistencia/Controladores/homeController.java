144c691cd1ada31bcfe856daeda8d29ab21bbace
kenyasuncin@gmail.com <kenyasuncin@gmail.com>
2025-08-19 23:03:02 -0600

    se hizo aplication.properties y en formlogin

--- Asistencia/src/main/java/com/esfe/Asistencia/Controladores/homeController.java
+++ Asistencia/src/main/java/com/esfe/Asistencia/Controladores/homeController.java
@@ -1,7 +1,11 @@
 package com.esfe.Asistencia.Controladores;
 
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/")
@@ -10,5 +14,17 @@ public class homeController {
     @RequestMapping
     public String Index(){
         return "home/index";
+    }
+
+    @GetMapping("/login" )
+    public String mostrarLogin() {
+        return "home/formLogin";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request){
+        SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
+        logoutHandler.logout(request, null, null);
+        return "redirect:/";
     }
 }
